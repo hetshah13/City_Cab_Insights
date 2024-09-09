d25bd223f3b75aa147d3ca9501e5b7f53d0e1c7f
hetshah13 <hetshah1309@gmail.com>
2024-09-10 00:23:25 +0530

    data wrangling commit

--- Pre-processing.r
+++ Pre-processing.r
@@ -13,5 +13,19 @@ View(dfs)
 
 ## variable understanding 
 unique_vendor = unique(dfs[[1]][["VendorID"]]);unique_vendor     # 1 2
-
+u_ploc = unique(dfs[[1]][["PULocationID"]]); u_ploc # 257 different location
+u_passenger = unique(dfs[[1]][["passenger_count"]]); u_passenger    # 0 To 9 ,NA
+u_ratecode = unique(dfs[[5]][["RatecodeID"]]); u_ratecode  # 1 TO 6 ,99,NA
+u_pay = unique(dfs[[1]][["payment_type"]]); u_pay    # 2 1 4 3 0
 summary(dfs[[1]])
+
+## Data wrangling
+#(1) data wrangling removing redundant recordes sequentially (first data is in negative and same record is in positive value.if i remove negative records it will automatically duplicate records will be removed.)
+
+flt_data = filtered_data <- dfs[[1]][dfs[[1]]$total_amount >= 0.01, ]
+View(flt_data)
+
+
+#(2) Remove rows where trip distance is 0 and PULocationID is equal to DOLocationID
+filtered_data <- dfs[[1]][!(dfs[[1]]$trip_distance == 0 & dfs[[1]]$PULocationID == dfs[[1]]$DOLocationID), ]
+View(filtered_data)
