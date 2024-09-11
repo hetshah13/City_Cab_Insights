5992ae730bc2f7d3dda7e394c68a8164b6a9fc60
hetshah13 <hetshah1309@gmail.com>
2024-09-11 21:57:25 +0530

    Data Cleaning Commit

--- Pre-processing.r
+++ Pre-processing.r
@@ -19,7 +19,7 @@ u_ratecode = unique(dfs[[5]][["RatecodeID"]]); u_ratecode  # 1 TO 6 ,99,NA
 u_pay = unique(dfs[[1]][["payment_type"]]); u_pay    # 2 1 4 3 0
 summary(dfs[[1]])
 
-## Data wrangling
+## Checking data 
 #(1) data wrangling removing redundant recordes sequentially from total amount (first data is in negative and same record is in positive value.if i remove negative records it will automatically duplicate records will be removed.)
 flt_data = filtered_data <- dfs[[1]][dfs[[1]]$total_amount >= 0.01, ]
 
@@ -39,6 +39,26 @@ dfs[[1]]$tpep_pickup_datetime <- as.POSIXct(dfs[[1]]$tpep_pickup_datetime)
 # Filter using subset function
 filtered_data <- subset(dfs[[1]], tpep_pickup_datetime >= as.POSIXct("2023-01-01 00:00:01") & tpep_pickup_datetime <= as.POSIXct("2023-02-01 08:00:00"))
 View(filtered_data)
-
 # Or filter using boolean indexing
 filtered_data <- df[df$tpep_pickup_datetime >= as.POSIXct("2023-01-01 00:00:51") & df$tpep_pickup_datetime <= as.POSIXct("2023-02-01 05:00:00"), ]
+
+# DATA CLEANING
+# Define a function to filter dataframe based on conditions
+filter_dataframe <- function(df) {
+  
+  # Remove rows where total_amount is less than 0.01
+  # Remove rows where fare_amount is less than 0.01 ,mta_tax is less than 0.00
+  df <- df[df$total_amount >= 0.01 & df$fare_amount >= 0.01 & df$mta_tax >= 0.00 & df$extra >= 0.00, ]
+  
+  # Remove rows where trip_distance is 0 and PULocationID is equal to DOLocationID
+  df <- df[!(df$trip_distance == 0 & df$PULocationID == df$DOLocationID), ]
+  
+  return(df)
+}
+
+# Apply the filter_dataframe function to each dataframe in the list
+flt_dfs <- lapply(dfs, filter_dataframe)
+
+# Downloading dataframe  after data cleaning
+# write_parquet(dataframe[[1]],"path where they store")
+write_parquet(flt_dfs[[1]],"D:/BDA/sem4/project/taxi_data/flt_1.parquet")
